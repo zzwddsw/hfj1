73770412faf84eefc0da3cc5adf35549b45d7393
Layne <2214626930@qq.com>
2023-07-06 09:26:15 +0800

    hot-fix commit 1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
 
     }
 }
