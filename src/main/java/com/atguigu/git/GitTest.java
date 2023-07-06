8f12639eeccbfd10295c1c1f5c3ea3352e4cf59a
Layne <2214626930@qq.com>
2023-07-06 09:25:10 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+
     }
 }
