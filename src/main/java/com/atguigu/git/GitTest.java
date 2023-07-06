34e0d50074c0524e881a47ecfc78d3f13a9059e8
Layne <2214626930@qq.com>
2023-07-06 09:54:58 +0800

    master commit test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("master git test ");
 
     }
 }
